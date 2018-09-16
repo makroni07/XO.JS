3071c30d836e4fcc60532d45669a6afb9bc0c17a
Makroni07 <mnaufal4567@gmail.com>
2018-09-17 05:36:46 +0700

    Belajar XO javasricpt

---
+++ XO.js
@@ -0,0 +1,15 @@
+function xo(str) {
+  if (str.split('o').length-1 !== str.split('x').length-1){
+  	return false;
+  }
+  else { 
+  	return true;
+  }
+}
+
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
